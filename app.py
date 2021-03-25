41fa36bf8b6a6d4d20dc346cec36428d7b926eb8
Nicklas <nicklascschmidt@nicklass-mbp.lan>
2021-03-24 19:15:25 -0700

    added logic to app.py to let user add and delete cities to db, added flash messages for user actions

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'thisisasecret123'
 
 db = SQLAlchemy(app)
 
@@ -12,23 +13,32 @@ class City(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(50), nullable=False)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-  if request.method == 'POST':
-    new_city = request.form.get('city')
-    if new_city:
-      new_city_obj = City(name=new_city)
-      db.session.add(new_city_obj)
-      db.session.commit()
+def get_weather_data(city):
+  url = f'http://api.openweathermap.org/data/2.5/weather?q={ city }&units=imperial&appid=e015ac40c1e51777f52433b0962155a4'
+  r = requests.get(url).json()
+  return r
 
+def capitalize_full_string(string):
+  if len(string) == 0:
+    return string
+    
+  words = str.split(string)
+  if len(words) == 0:
+    return words[0]
+  else:
+    new_words = []
+    for word in words:
+      new_words.append(word[:1].upper() + word[1:].lower())
+    return ' '.join(new_words)
 
+@app.route('/')
+def index_get():
   cities = City.query.all()
 
   weather_data = []
 
   for city in cities:
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=e015ac40c1e51777f52433b0962155a4'
-    r = requests.get(url.format(city.name)).json()
+    r = get_weather_data(city.name)
 
     weather = {
       'city': city.name,
@@ -38,7 +48,43 @@ def index():
     }
 
     weather_data.append(weather)
-  
 
   return render_template('weather.html', weather_data=weather_data)
 
+
+@app.route('/', methods=['POST'])
+def index_post():
+  err_msg = ''
+  new_city = capitalize_full_string(request.form.get('city'))
+
+  if new_city:
+    is_existing_city = City.query.filter(City.name.ilike(new_city)).first()
+
+    if not is_existing_city:
+      new_city_data = get_weather_data(new_city)
+
+      if new_city_data['cod'] == 200:
+        new_city_obj = City(name=new_city)
+      
+        db.session.add(new_city_obj)
+        db.session.commit()
+      else:
+        err_msg = 'City does not exist in the world!'
+    else:
+        err_msg = 'City already exists in the database!'
+
+  if err_msg:
+    flash(err_msg, 'error')
+  else:
+    flash('City added successfully')
+
+  return redirect(url_for('index_get'))
+
+@app.route('/delete/<name>')
+def delete_city(name):
+  city = City.query.filter(City.name.ilike(name)).first()
+  db.session.delete(city)
+  db.session.commit()
+
+  flash(f'Successfully deleted { city.name }', 'success')
+  return redirect(url_for('index_get'))
